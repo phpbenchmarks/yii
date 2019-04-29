ecfe4744711d796ac792ff1dcbed85ca0e476b90
Steevan <steevan.barboyon@gmail.com>
2019-04-29 19:08:52 +0200

    Add stats.php

--- web/index.php
+++ web/index.php
@@ -9,3 +9,5 @@ require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
 
 $config = require __DIR__ . '/../config/web.php';
 (new yii\web\Application($config))->run();
+
+// require phpbenchmarks stats.php here when needed
